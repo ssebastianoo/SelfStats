3d2e1f338e1cd673b6bd18d5675c95cde71e4d0e
ssebastianoo <sebastianogirotto05@gmail.com>
2024-05-27 18:10:21 +0200

    load project from localStorage

--- src/routes/project/[id]/+page.server.ts
+++ src/routes/project/[id]/+page.server.ts
@@ -1,68 +1,12 @@
 import type { PageServerLoad } from './$types';
 import { error } from '@sveltejs/kit';
-import { supabase } from '$lib/server/supabase';
-import type { ProjectT } from '$lib/types';
-import { getUser } from '$lib/server/utils';
 
-export const load = (async ({ params, cookies }) => {
-	const { user, error: userError } = getUser(cookies);
-	if (userError || !user) {
-		throw error(401, userError);
-	}
-
-	const userId = user.sub;
-
-	let project: ProjectT;
-
-	const { data: projectData } = await supabase
-		.from('projects')
-		.select('*')
-		.eq('user_id', userId)
-		.eq('id', params.id);
-
-	const { data: descriptorsData } = await supabase
-		.from('descriptors')
-		.select('*')
-		.eq('project_id', params.id);
-
-	const { data: datasData } = await supabase
-		.from('data')
-		.select('*')
-		.eq('project_id', params.id)
-		.order('created_at', { ascending: false });
-
-	const { data: valuesData } = await supabase
-		.from('values')
-		.select('*')
-		.eq('project_id', params.id);
-
-	if (projectData && projectData.length > 0) {
-		project = { ...projectData[0], descriptors: descriptorsData };
-
-		if (datasData) {
-			project.data = datasData;
-
-			if (valuesData && valuesData.length > 0) {
-				for (const value of valuesData) {
-					const linkedData = project.data.find((data) => data.id === value.data_id);
-					if (linkedData) {
-						if (!linkedData.values) linkedData.values = [];
-						linkedData.values.push(value);
-					}
-				}
-			} else {
-				for (const data of project.data) {
-					data.values = [];
-				}
-			}
-		} else {
-			project.data = [];
-		}
-	} else {
-		throw error(404, 'Project not found');
+export const load = (async ({ params }) => {
+	if (/^\d+$/.test(params.id) === false) {
+		throw error(404, 'Not found');
 	}
 
 	return {
-		project
+		project_id: parseInt(params.id)
 	};
 }) satisfies PageServerLoad;
--- src/routes/project/[id]/+page.svelte
+++ src/routes/project/[id]/+page.svelte
@@ -4,17 +4,39 @@
 	import { Textarea } from '$lib/components/ui/textarea';
 	import { Button } from '$lib/components/ui/button';
 	import { Label } from '$lib/components/ui/label';
-	import type { DescriptorT } from '$lib/types';
+	import type { DescriptorT, ProjectT } from '$lib/types';
 	import { project } from '$lib/store';
 	import { goto } from '$app/navigation';
 	import { alert } from '$lib/store';
+	import { onMount } from 'svelte';
 
 	export let data;
 
-	$project = data.project;
-
 	let editing = false;
 	let openSection: 'edit' | 'create' | null = null;
+
+	onMount(() => {
+		let projects: ProjectT[] = [];
+		if (localStorage.getItem('projects')) {
+			projects = JSON.parse(localStorage.getItem('projects')!);
+			const _project = projects.find((p) => p.id === data.project_id);
+			if (_project) {
+				$project = _project;
+				return;
+			}
+		}
+	});
+
+	function getProjects() {
+		if (!localStorage.getItem('projects')) {
+			localStorage.setItem('projects', JSON.stringify([]));
+		}
+		return JSON.parse(localStorage.getItem('projects')!) as ProjectT[];
+	}
+
+	function setProjects(projects: ProjectT[]) {
+		localStorage.setItem('projects', JSON.stringify(projects));
+	}
 
 	async function editProject(e: Event) {
 		const target = e.target as HTMLFormElement;
@@ -26,24 +48,19 @@
 			description = null;
 		}
 
-		const res = await fetch(`/api/projects`, {
-			method: 'PATCH',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				name,
-				description,
-				project_id: $project.id
-			})
+		$project.name = name;
+		$project.description = description;
+
+		let projects = getProjects();
+
+		projects = projects.map((p) => {
+			if (p.id === $project.id) {
+				return $project;
+			}
+			return p;
 		});
 
-		if (!res.ok) {
-			return console.error('Failed to edit project');
-		}
-
-		const json = await res.json();
-		$project = json;
+		setProjects(projects);
 	}
 
 	async function editDescriptors(e: Event) {
@@ -167,162 +184,164 @@
 	}
 </script>
 
-{#if !editing}
-	<Project
-		on:edit={() => {
-			editing = true;
-		}}
-	/>
-{:else}
-	<div class="flex gap-3 items-center">
-		<h2 class="text-xl">Editing project</h2>
-		<Button
-			variant="outline"
-			size="sm"
-			on:click={() => {
-				editing = false;
+{#if $project}
+	{#if !editing}
+		<Project
+			on:edit={() => {
+				editing = true;
 			}}
-		>
-			Close
-		</Button>
-		<Button variant="destructive" size="sm" on:click={deleteProject}>Delete project</Button>
-	</div>
-	<hr class="my-4" />
-	<form on:submit|preventDefault={editProject} class="flex flex-col gap-2">
-		<div class="flex gap-2">
-			<Label for="name" class="w-28">Name</Label>
-			<Input
-				id="name"
-				type="text"
-				name="project_name"
-				placeholder="Statistics Project"
-				value={$project.name}
-				required
-			/>
+		/>
+	{:else}
+		<div class="flex gap-3 items-center">
+			<h2 class="text-xl">Editing project</h2>
+			<Button
+				variant="outline"
+				size="sm"
+				on:click={() => {
+					editing = false;
+				}}
+			>
+				Close
+			</Button>
+			<Button variant="destructive" size="sm" on:click={deleteProject}>Delete project</Button>
 		</div>
-		<div class="flex gap-2">
-			<Label for="description" class="w-28">Description</Label>
-			<Textarea
-				id="description"
-				name="description"
-				placeholder="My really cool statistics project."
-				value={$project.description}
-			/>
-		</div>
-		<div class="flex justify-end">
-			<Button type="submit" size="sm" variant="default" class="w-20">Save</Button>
-		</div>
-	</form>
-	<hr class="my-4" />
-	<h2 class="text-lg">Descriptors</h2>
+		<hr class="my-4" />
+		<form on:submit|preventDefault={editProject} class="flex flex-col gap-2">
+			<div class="flex gap-2">
+				<Label for="name" class="w-28">Name</Label>
+				<Input
+					id="name"
+					type="text"
+					name="project_name"
+					placeholder="Statistics Project"
+					value={$project.name}
+					required
+				/>
+			</div>
+			<div class="flex gap-2">
+				<Label for="description" class="w-28">Description</Label>
+				<Textarea
+					id="description"
+					name="description"
+					placeholder="My really cool statistics project."
+					value={$project.description}
+				/>
+			</div>
+			<div class="flex justify-end">
+				<Button type="submit" size="sm" variant="default" class="w-20">Save</Button>
+			</div>
+		</form>
+		<hr class="my-4" />
+		<h2 class="text-lg">Descriptors</h2>
 
-	<div class="my-4">
-		<Button
-			on:click={() => {
-				openSection = 'create';
-			}}
-			size="sm"
-			variant={openSection === 'create' ? 'default' : 'outline'}>Create descriptor</Button
-		>
-		<Button
-			on:click={() => {
-				openSection = 'edit';
-			}}
-			size="sm"
-			variant={openSection === 'edit' ? 'default' : 'outline'}>Edit descriptors</Button
-		>
-	</div>
+		<div class="my-4">
+			<Button
+				on:click={() => {
+					openSection = 'create';
+				}}
+				size="sm"
+				variant={openSection === 'create' ? 'default' : 'outline'}>Create descriptor</Button
+			>
+			<Button
+				on:click={() => {
+					openSection = 'edit';
+				}}
+				size="sm"
+				variant={openSection === 'edit' ? 'default' : 'outline'}>Edit descriptors</Button
+			>
+		</div>
 
-	{#if openSection === 'edit'}
-		<form on:submit|preventDefault={editDescriptors} class="flex flex-col gap-2">
-			{#each $project.descriptors as descriptor}
+		{#if openSection === 'edit'}
+			<form on:submit|preventDefault={editDescriptors} class="flex flex-col gap-2">
+				{#each $project.descriptors as descriptor}
+					<div class="flex gap-2">
+						<Label for={'descriptor_name_' + descriptor.id} class="w-28">Name</Label>
+						<Input
+							id={'descriptor_name_' + descriptor.id}
+							name={'descriptor_name_' + descriptor.id}
+							placeholder="My really cool descriptor"
+							value={descriptor.name}
+						/>
+					</div>
+					<div class="flex gap-2">
+						<Label for={'descriptor_description_' + descriptor.id} class="w-28">Description</Label>
+						<Input
+							id={'descriptor_description_' + descriptor.id}
+							name={'descriptor_description_' + descriptor.id}
+							placeholder="My really cool descriptor"
+							value={descriptor.description}
+						/>
+					</div>
+
+					<div class="flex gap-2">
+						<Label for={'descriptor_type_' + descriptor.id} class="w-28">Type</Label>
+
+						<select
+							name={'descriptor_type_' + descriptor.id}
+							id={'descriptor_type_' + descriptor.id}
+							class="h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm
+			ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium
+			placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2
+			focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+							required
+						>
+							<option value="text" selected={descriptor.type === 'text'}>Text</option>
+							<option value="number" selected={descriptor.type === 'number'}>Number</option>
+						</select>
+					</div>
+					<div class="flex justify-end">
+						<Button
+							variant="destructive"
+							size="sm"
+							on:click={async () => {
+								await deleteDescriptor(descriptor);
+							}}>Delete</Button
+						>
+					</div>
+
+					<hr class="my-4" />
+				{/each}
+				<div class="flex justify-end">
+					<Button type="submit" size="sm" variant="default" class="w-20">Save</Button>
+				</div>
+			</form>
+		{/if}
+		{#if openSection === 'create'}
+			<form on:submit|preventDefault={createDescriptor} class="flex flex-col gap-2">
 				<div class="flex gap-2">
-					<Label for={'descriptor_name_' + descriptor.id} class="w-28">Name</Label>
+					<Label for="name" class="w-28">Name</Label>
 					<Input
-						id={'descriptor_name_' + descriptor.id}
-						name={'descriptor_name_' + descriptor.id}
-						placeholder="My really cool descriptor"
-						value={descriptor.name}
+						id="name"
+						type="text"
+						name="descriptor_name"
+						placeholder="Descriptor name"
+						required
 					/>
 				</div>
 				<div class="flex gap-2">
-					<Label for={'descriptor_description_' + descriptor.id} class="w-28">Description</Label>
-					<Input
-						id={'descriptor_description_' + descriptor.id}
-						name={'descriptor_description_' + descriptor.id}
-						placeholder="My really cool descriptor"
-						value={descriptor.description}
-					/>
+					<Label for="description" class="w-28">Description</Label>
+					<Textarea id="description" name="description" placeholder="Descriptor description" />
 				</div>
 
 				<div class="flex gap-2">
-					<Label for={'descriptor_type_' + descriptor.id} class="w-28">Type</Label>
-
+					<Label for="type" class="w-28">Type</Label>
 					<select
-						name={'descriptor_type_' + descriptor.id}
-						id={'descriptor_type_' + descriptor.id}
+						name="type"
+						id="type"
 						class="h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm
 			ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium
 			placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2
 			focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
 						required
 					>
-						<option value="text" selected={descriptor.type === 'text'}>Text</option>
-						<option value="number" selected={descriptor.type === 'number'}>Number</option>
+						<option value="text">Text</option>
+						<option value="number">Number</option>
 					</select>
 				</div>
 				<div class="flex justify-end">
-					<Button
-						variant="destructive"
-						size="sm"
-						on:click={async () => {
-							await deleteDescriptor(descriptor);
-						}}>Delete</Button
-					>
+					<Button type="submit" size="sm" variant="default" class="w-20">Save</Button>
 				</div>
-
-				<hr class="my-4" />
-			{/each}
-			<div class="flex justify-end">
-				<Button type="submit" size="sm" variant="default" class="w-20">Save</Button>
-			</div>
-		</form>
-	{/if}
-	{#if openSection === 'create'}
-		<form on:submit|preventDefault={createDescriptor} class="flex flex-col gap-2">
-			<div class="flex gap-2">
-				<Label for="name" class="w-28">Name</Label>
-				<Input
-					id="name"
-					type="text"
-					name="descriptor_name"
-					placeholder="Descriptor name"
-					required
-				/>
-			</div>
-			<div class="flex gap-2">
-				<Label for="description" class="w-28">Description</Label>
-				<Textarea id="description" name="description" placeholder="Descriptor description" />
-			</div>
-
-			<div class="flex gap-2">
-				<Label for="type" class="w-28">Type</Label>
-				<select
-					name="type"
-					id="type"
-					class="h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm
-			ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium
-			placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2
-			focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
-					required
-				>
-					<option value="text">Text</option>
-					<option value="number">Number</option>
-				</select>
-			</div>
-			<div class="flex justify-end">
-				<Button type="submit" size="sm" variant="default" class="w-20">Save</Button>
-			</div>
-		</form>
+			</form>
+		{/if}
 	{/if}
 {/if}
