fa97c04cbca5c8402ac2b8392239793285968d58
ssebastianoo <sebastianogirotto05@gmail.com>
2024-05-28 11:56:24 +0200

    remove auth stuff

--- src/lib/server/supabase.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import { env } from '$env/dynamic/public';
-import { env as envServer } from '$env/dynamic/private';
-
-export const supabase = createClient(env.PUBLIC_SUPABASE_URL!, envServer.PRIVATE_SERVICE_ROLE);
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -56,33 +56,6 @@ export const flyAndScale = (
 	};
 };
 
-export function setCookie(cname: string, cvalue: string, exdays: number) {
-	const d = new Date();
-	d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-	const expires = 'expires=' + d.toUTCString();
-	document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
-}
-
-export function getCookie(cname: string) {
-	const name = cname + '=';
-	const decodedCookie = decodeURIComponent(document.cookie);
-	const ca = decodedCookie.split(';');
-	for (let i = 0; i < ca.length; i++) {
-		let c = ca[i];
-		while (c.charAt(0) == ' ') {
-			c = c.substring(1);
-		}
-		if (c.indexOf(name) == 0) {
-			return c.substring(name.length, c.length);
-		}
-	}
-	return null;
-}
-
-export function deleteCookie(cname: string) {
-	document.cookie = cname + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-}
-
 export function getProjects() {
 	if (!localStorage.getItem('projects')) {
 		localStorage.setItem('projects', JSON.stringify([]));
