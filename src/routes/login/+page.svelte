915ce4ce0a736a3524e3271e5f76f27f9ba4703e
ssebastianoo <sebastianogirotto05@gmail.com>
2024-06-15 01:18:31 +0200

    push bug

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { SignIn } from '@auth/sveltekit/components';
+	import { providerMap } from '../../auth';
+</script>
+
+<div>
+	<div>
+		<h2>
+			<span>Company</span>
+		</h2>
+		<div>
+			{#each providerMap as provider}
+				<SignIn provider={provider.id} signInPage="signin" className="w-full">
+					<div slot="submitButton">
+						<span>
+							Signin with {provider.name}
+						</span>
+					</div>
+				</SignIn>
+			{/each}
+		</div>
+	</div>
+</div>
